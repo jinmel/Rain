760b6799c1856b36b971ff9dd8ea309297b26c8b
icothos <icothos@gmail.com>
2015-05-07 23:39:24 +0900

    dropbox clould #1.0
    일단 작성해서 올림

--- src/client/RainLib/ports/dropbox.py
+++ src/client/RainLib/ports/dropbox.py
@@ -1,8 +1,114 @@
+import webbrowser
+import BaseHTTPServer
+from dropbox.client import DropboxOAuth2Flow, DropboxClient
+import urllib
 from .. import clouddrive
 
 
 class DropBox(clouddrive.CloudDrive):
-    def __init__(self):
-        pass
+    
+    def __init__(self, access_token=None):
+        if access_token is not None :
+                self.client=DropboxClient(access_token)
+        self.access_token = access_token
+        super.__init__(self,access_token)
 
+        
+    def auth(self):
+        app_key = 'knbyx2adg14kkn5'
+        app_secret = 'kh3ulgqry8jffqp'
+        global Auth_DROPMytoken
+        global Auth_Drop_Running_true
+
+        def keep_running():
+            global Auth_Drop_Running_true
+            return Auth_Drop_Running_true
+
+        class AuthHandler(BaseHTTPServer.BaseHTTPRequestHandler) :
+            def do_HEAD(s):
+                s.send_response(200)
+                s.send_header("Content-type","text/html")
+                s.end_headers()
+            def do_GET(s):
+                s.send_response(200)
+                s.send_header("Content-type","text/html")
+                s.end_headers()
+                if s.path.find("code=") !=-1:
+                    global Auth_DROPMytoken
+                    global Auth_Drop_Running_true
+                    Auth_DROPMytoken=s.path
+                    Auth_Drop_Running_true=False
+                    s.wfile.write("ok see command line")
+                    return
+
+        class MYrequest : 
+
+            def __init__(self, url):
+                self.url=url			
+
+            def get (self,mystring) :
+                if self.url.find(mystring) ==-1 :
+                    return
+                item=self.url.split(mystring+"=")[1]
+                if item.find("&")!=-1 :
+                    item=item.split("&")[0]
+                return urllib.unquote(item).decode()
+ 	
+
+
+        my_session={}
+        Auth_Drop_Running_true=True
+        flow = DropboxOAuth2Flow(app_key, app_secret,redirect_url,my_session,"dropbox-auth-csrf-token")    
+
+        authorize_url = flow.start() 
+        webbrowser.open(authorize_url)
+
+        try:
+            httpd= BaseHTTPServer.HTTPServer(("",8080),AuthHandler)
+            while keep_running():
+            	httpd.handle_request()
+        except KeyboardInterrupt :
+            pass
+        httpd.server_close()
+    
+        token, user_id, url_state = flow.finish(MYrequest(Auth_DROPMytoken))
+        self.access_token=token
+        super.access_token=token
+        self.client=DropboxClient(access_token)
+        return token
+    
+
+    def read(self, filename):
+        try :
+            f = open(filename, 'rb',overwrite)
+            response = self.client.put_file(filename, f)
+            f.close()
+        except :
+            return -1
+        return 1
+
+    def write(self, filename):
+        try:
+            out = open("/tmp"+filename, 'wb')
+            with self.client.get_file(remote_file) as f:
+                out.write(f.read())
+            out.close()
+        except :
+            return -1
+        return 1
+        
+    def mkdir(self, dirname):
+        try :
+            self.client.file_create_folder(dirname)
+        except :
+            return -1
+        return 1
+        
+    def delete(self, filename):
+        try:
+            self.client.file_delete(filename)
+        except :
+                return -1
+        return 1
+    
 
