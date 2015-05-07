169f3fb9f666ce65162f34fc13d7a1a2a5fb8460
SoyeonPark <thdusdl1219@postech.ac.kr>
2015-05-07 08:11:28 -0700

    google drive auth

--- src/client/RainLib/ports/googledrive.py
+++ src/client/RainLib/ports/googledrive.py
@@ -1,9 +1,50 @@
 from .. import clouddrive
-
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from pydrive.files import GoogleDriveFile
+from os.path import os
 
 __name__ = "Google Drive"
 
 
 class GoogleDrive(clouddrive.CloudDrive):
-    def __init__(self):
+    def __init__(self, access_token=None, drive=None):
+        super.__init__(self.access_token)
         pass
+
+    def auth(self):
+        gauth = GoogleAuth()
+        if(self.access_token == None) :
+            gauth.LocalWebserverAuth()
+            gauth.SaveCredentialsFile("oauth.txt")
+            f = open("oauth.txt")
+            self.access_token = f.readline()
+            f.close()
+            os.remove("oauth.txt")
+        else :
+            f = open("oauth.txt", "w")
+            f.write(self.access_token)
+            f.close()
+            gauth.LoadCredentials(self.access_token)
+            os.remove("oauth.txt")
+            
+        self.drive = GoogleDrive(gauth)
+    def read(self, filename):
+
+    def write(self, filename):
+
+    def mkdir(self, dirname):
+        for i in len(dirname.split("/"))-1 :
+            
+        if os.path.isdir(dirname):
+            file_new = drive.CreateFile({'title' : dirname, 'mimeType' : "application/vnd.google-apps.folder",'parents':[{'kind':parents['kind'], 'id':parents['id']}]})
+        else :
+            return -1
+        file_new.Upload()
+        return 1
+
+    def delete(self, filename):
+
+
+    def find_file_id(self, filename):
+    
