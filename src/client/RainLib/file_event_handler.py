54919b393d2286abd87f592a26deb9d426e97a45
Jin Suk Park <totorovirus@gmail.com>
2015-05-27 21:59:25 +0900

    run.py refactored and RainEventHandler now takes RainDrive instance

--- src/client/RainLib/file_event_handler.py
+++ src/client/RainLib/file_event_handler.py
@@ -1,17 +1,35 @@
 from watchdog.events import FileSystemEventHandler
 
+
 class RainFileSystemEventHandler(FileSystemEventHandler):
+    #TODO: implement this according to metafile.xml
+
+    def __init__(self, rain_drive):
+        self.rain_drive = rain_drive
+
     def on_created(self, event):
+        print 'create', event.src_path
         pass
 
     def on_deleted(self, event):
+        print 'delete', event.src_path
         pass
 
     def on_modified(self, event):
+        print 'modify', event.src_path
         pass
 
     def on_moved(self, event):
+        print 'move', event.src_path
         pass
 
     def on_any_event(self, event):
+        print event.event_type
         pass
+
+
+
+
+
+
+
--- src/client/run.py
+++ src/client/run.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python
 
 import xml.etree.ElementTree as ET
-from RainLib.ports import *
+from RainLib.ports.dropbox import DropBox
+from RainLib.ports.googledrive import GoogleDrive
+from RainLib.ports.box import Box
 from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+from RainLib.file_event_handler import RainFileSystemEventHandler
+from RainLib.utils import RainMetaFileAdapter
+from RainLib.raindrive import RainDrive
+import logging
+import time
 
-META_FILENAME = 'metafile.xml'
-SUPPORTED_CLOUDS = [dropbox.__name__, box.__name__, googledrive.__name__]
+SUPPORTED_CLOUDS = [DropBox.name, GoogleDrive.name, Box.name]
 
 if __name__ == "__main__":
     #TODO: Read metafile.xml initialize cloud drive instances
-
-    dropbox_instance = dropbox.DropBox()
-    box_instance = box.Box()
-    googledrive_instance = googledrive.GoogleDrive()
+    mfa = RainMetaFileAdapter('metafile.xml')
 
     print "=== Rain - Cloud Unification ==="
     print "1. Add Cloud Drives"
     print "2. Start Rain Daemon"
-    choice = raw_input('select>')
+    print "3. Debug"
+    choice = int(raw_input('select > '))
+    #choice = 3
 
-    if choice == '1':
+    if choice == 1:
         #TODO: open meta file and check what clouds are registered
-        metafile = ET.parse(META_FILENAME)
-        root = metafile.getroot()
-        cloud_registered = []
-        for cloud in root.findall('cloud'):
-            name = cloud.find('name')
-            cloud_registered.append(name.text)
-
-        cloud_not_registered = list(set(SUPPORTED_CLOUDS).difference(set(cloud_registered)))
-
+        cloud_registered = mfa.get_all_cloud_name()
+        cloud_not_registered = list(set(SUPPORTED_CLOUDS) - set(cloud_registered))
         menu = {}
         for i in xrange(len(cloud_not_registered)):
             menu[i + 1] = cloud_not_registered[i]
@@ -37,24 +36,46 @@ if __name__ == "__main__":
         for k, v in menu.iteritems():
             print str(k) + '. ' + v
 
-        choice = int(raw_input('select>'))
+        choice = int(raw_input('select > '))
         #Authentification for each clouds
         access_token = ''
-        if menu[choice] == dropbox.__name__:
-            access_token = dropbox_instance.auth()
-        elif menu[choice] == googledrive.__name__:
-            access_token = googledrive_instance.auth()
-        elif menu[choice] == box_instance.__name__:
-            access_token = box_instance.auth()
+        if menu[choice] == DropBox.name:
+            access_token = DropBox.auth()
+        elif menu[choice] == GoogleDrive.name:
+            access_token = GoogleDrive.auth()
+        elif menu[choice] == Box.name:
+            access_token = Box.auth()
 
-        new_cloud = ET.SubElement(metafile.getroot(), 'cloud')
-        ET.SubElement(new_cloud, 'name').text = menu[choice]
-        ET.SubElement(new_cloud, 'access_token').text = access_token
-        metafile.write(META_FILENAME)
+        mfa.add_cloud(menu[choice], access_token)
+        mfa.write()
         print 'Metafile updated'
-    elif choice == '2':
-        pass
+    elif choice == 2:
+        rdrive = RainDrive(mfa)
+        cloud_names = mfa.get_all_cloud_name()
+        for cloud_name in cloud_names:
+            if cloud_name == DropBox.name:
+                rdrive.add_cloud(DropBox(mfa.get_cloud_access_token(cloud_name)))
+            elif cloud_name == GoogleDrive.name:
+                rdrive.add_cloud(GoogleDrive(mfa.get_cloud_access_token(cloud_name)))
+            elif cloud_name == Box.name:
+                rdrive.add_cloud(Box(mfa.get_cloud_access_token(cloud_name)))
+            else:
+                raise Exception('Unknown cloud service name: ' + cloud_name)
 
-
-
-
+        observer = Observer()
+        event_handler = RainFileSystemEventHandler(rdrive)
+        observer.schedule(event_handler, ".", recursive=True)
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
+    elif choice == 3:
+        mfa.add_file('DropBox', './data3.txt', '/Rain/data3.txt', '1024')
+        print mfa.get_local_file_list('DropBox')
+        print mfa.get_remote_file_list('DropBox')
+        print mfa.get_cloud_name_by_local_filename('./data3.txt')
+        print mfa.get_all_cloud_name()
+        print mfa.get_file_map('DropBox')
