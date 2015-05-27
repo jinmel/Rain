279ffe2d479728b5cee626a46ec787649c87b5f2
Jin Suk Park <totorovirus@gmail.com>
2015-05-27 21:57:12 +0900

    auth -> static method, capacity method added

--- src/client/RainLib/clouddrive.py
+++ src/client/RainLib/clouddrive.py
@@ -2,7 +2,8 @@ class CloudDrive(object):
     def __init__(self, access_token=None):
         self.access_token = access_token
 
-    def auth(self):
+    @staticmethod
+    def auth():
         #TODO : Do authentification and return access token
         raise NotImplementedError()
 
@@ -11,7 +12,7 @@ class CloudDrive(object):
         #TODO : Read filename(remote_filename) from cloud drive and return contets on success or -1 on failure
         raise NotImplementedError()
 
-    def write(self, filename):
+    def write(self, filename, data):
         #TODO : Write filename(remote_filename) from cloud drive and return 1 on success or -1 on failure
         raise NotImplementedError()
 
@@ -21,6 +22,10 @@ class CloudDrive(object):
 
     def delete(self, filename):
         #TODO : delete filename(remote_filename) from cloud drive return 1 on success or -1 on failure
+        raise NotImplementedError()
+
+    def capacity(self):
+        #TODO : return remaining capacity of cloud drive
         raise NotImplementedError()
 
 
